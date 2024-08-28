eea9ba663921539e46c3d5d602f6245fa21582c7
Felix Laniyan <flaniyanjr@gmail.com>
2024-08-28 19:13:25 -0400

    search working

--- src/AdList.js
+++ src/AdList.js
@@ -1,20 +1,50 @@
 import AdCard from "./AdCard"
+import { useState } from "react"
 
 function AdList({adData}) {
 
-    const facebookList= adData.facebook_ads
-    const twitterList= adData.twitter_ads
-    const snapchatList= adData.snapchat_ads
+    const [searchInput, setSearchInput]= useState('')
+
+    function handleSearchInput(e) {
+        setSearchInput(e.target.value)
+    }
+    
+    console.log(adData.facebook_ads)
+
     const googleList= adData.google_analytics
 
-    const masterList= [...facebookList, ...twitterList, ...snapchatList]
+    const masterList= [...adData.facebook_ads, ...adData.twitter_ads, ...adData.snapchat_ads]
 
-    const adCards= masterList.map(adObj => {
+    console.log(masterList)
+
+
+    const filteredAds= masterList.filter(adObj => {
+        return (adObj.campaign_name || adObj.campaign).toLowerCase().includes(searchInput.toLowerCase())
+    })
+
+    console.log(filteredAds)
+
+    const adCards= filteredAds.map(adObj => {
         return <AdCard adObj= {adObj} googleList= {googleList}/>
     })
 
     return(
         <div className= 'card-container'>
+            <div className= 'search-sort-container'>
+                <div className= 'container'>
+                <input
+                        type="text"
+                        className="search"
+                        placeholder="Search campaign name..."
+                        value={searchInput}
+                        onChange={handleSearchInput}
+                    />
+                </div>
+                <div className= 'container'>
+                    
+                </div>
+
+            </div>
             {adCards}
         </div>
     )
