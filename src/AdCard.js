467e74d7ab02842784f09d992a66b699d265f5f3
Felix Laniyan <flaniyanjr@gmail.com>
2024-08-28 16:49:50 -0400

    all data except results rendering on ad card

--- src/AdCard.js
+++ src/AdCard.js
@@ -1,7 +1,13 @@
 function AdCard({adObj}) {
     return(
         <div className="card-box">
-            <p>{adObj.campaign_name || adObj.campaign || adObj.utm_campaign}</p>
+            <p>Campain: {adObj.campaign_name || adObj.campaign || adObj.utm_campaign}</p>
+            <p>Adset: {adObj.media_buy_name || adObj.ad_group || adObj.ad_squad_nam}</p>
+            <p>Creative: {adObj.ad_name || adObj.image_name || adObj.creative_name}</p>
+            <p>Spend: {adObj.spend || adObj.cost}</p>
+            <p>Impressions: {adObj.impressions}</p>
+            <p>Clicks: {adObj.clicks || adObj.post_clicks}</p>
+            <p>Results: </p>
         </div>
     )
 }
