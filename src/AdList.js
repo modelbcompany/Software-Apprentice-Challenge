26b2a9e58fa58a1adee766f0be73b0b2cf060231
Felix Laniyan <flaniyanjr@gmail.com>
2024-08-29 00:02:55 -0400

    sort working

--- src/AdList.js
+++ src/AdList.js
@@ -2,27 +2,43 @@ import AdCard from "./AdCard"
 import { useState } from "react"
 
 function AdList({adData}) {
-
     const [searchInput, setSearchInput]= useState('')
+    const [sortInput, setSortInput]= useState('')
 
     function handleSearchInput(e) {
         setSearchInput(e.target.value)
     }
+
+    function handleSort(e) {
+        setSortInput(e.target.value)
+    }
     
-    console.log(adData.facebook_ads)
+    // console.log(Array.isArray(adData.facebook_ads))
 
     const googleList= adData.google_analytics
 
     const masterList= [...adData.facebook_ads, ...adData.twitter_ads, ...adData.snapchat_ads]
 
-    console.log(masterList)
+    // console.log(masterList)
 
+    let sortedAds= []
 
-    const filteredAds= masterList.filter(adObj => {
+    switch(sortInput) {
+        case '':
+            sortedAds= masterList
+            break
+        case 'ascending':
+            sortedAds= masterList.sort((a,b) => (a.spend || a.cost) - (b.spend || b.cost))
+            break
+        case 'descending':
+            sortedAds= masterList.sort((a,b) => (b.spend || b.cost) - (a.spend || a.cost))
+    }
+
+    const filteredAds= sortedAds.filter(adObj => {
         return (adObj.campaign_name || adObj.campaign).toLowerCase().includes(searchInput.toLowerCase())
     })
 
-    console.log(filteredAds)
+    // console.log(filteredAds)
 
     const adCards= filteredAds.map(adObj => {
         return <AdCard adObj= {adObj} googleList= {googleList}/>
@@ -32,7 +48,7 @@ function AdList({adData}) {
         <div className= 'card-container'>
             <div className= 'search-sort-container'>
                 <div className= 'container'>
-                <input
+                    <input
                         type="text"
                         className="search"
                         placeholder="Search campaign name..."
@@ -41,9 +57,13 @@ function AdList({adData}) {
                     />
                 </div>
                 <div className= 'container'>
-                    
+                    <label id= 'sort-text'>Sort by spend:</label>
+                        <select name="sort" onChange={handleSort} id='sort-box'>
+                            <option value=''></option>
+                            <option value='ascending'>ascending</option>
+                            <option value='descending'>descending</option>
+                        </select>
                 </div>
-
             </div>
             {adCards}
         </div>
